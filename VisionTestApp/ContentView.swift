9eeaf9f98123ee0d8dfcb0beed6c498663870f18
Smrevilo <olivermstege@gmail.com>
2025-02-18 17:11:36 +0100

    Graphs and multiple views

--- VisionTestApp/ContentView.swift
+++ VisionTestApp/ContentView.swift
@@ -6,24 +6,84 @@
 //
 
 import SwiftUI
+import Charts
+
+struct DataPoint: Identifiable {
+    let id = UUID()
+    let day: String
+    let value: Double
+}
+
+let sampleData: [DataPoint] = [
+    DataPoint(day: "Mon", value: 5.0),
+    DataPoint(day: "Tue", value: 7.3),
+    DataPoint(day: "Wed", value: 4.3),
+    DataPoint(day: "Thu", value: 5.3),
+    DataPoint(day: "Fri", value: 6.3),
+    DataPoint(day: "Sat", value: 7.0),
+    DataPoint(day: "Sun", value: 7.6)
+    
+]
+
 
 struct ContentView: View {
     
-    @State private var buttonText = "Hello, Vision Pro!"
-    @State private var tapped = false
+    @State private var goToNext = false
     
     var body: some View {
-        Text(buttonText)
-            .font(.largeTitle)
-            .bold()
-            .position(x: 400, y: 700)
-        Button("Change Text") {
-            buttonText = tapped ? "Hello, Vision Pro!" : "Button was tapped!"
-            tapped = !tapped
-            print("Button was TAPPED")
+        NavigationStack {
+            VStack {
+                Text("Welcome to Vision Pro!")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                
+                Button("Go to second screen") {
+                    goToNext = true
+                }
+            }
+            .navigationDestination(isPresented: $goToNext) {
+                SecondView()
+            }
+            .navigationTitle("Main Screen")
         }
     }
 }
+
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Text("This is the second screen!")
+                .font(.title)
+                .padding()
+            
+            NavigationLink("Go to Third Screen", destination: GraphView())
+                .padding()
+                .buttonStyle(.bordered)
+            
+            Button("Go Back") {
+                dismiss()
+            }
+        }
+        .navigationTitle("Second Screen")
+    }
+}
+
+struct GraphView: View {
+    var body: some View {
+        Chart(sampleData) {
+            dataPoint in BarMark(
+                x: .value("Day", dataPoint.day),
+                y: .value("value", dataPoint.value)
+            )
+        }
+        .frame(height: 300)
+        .padding()
+    }
+}
+
 
 #Preview {
     ContentView()
