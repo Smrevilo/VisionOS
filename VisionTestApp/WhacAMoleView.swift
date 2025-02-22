666c5032e4b1df885417eed717194aa8a4bbb560
Smrevilo <olivermstege@gmail.com>
2025-02-22 10:11:56 +0100

    Whac-A-Mole m

--- VisionTestApp/WhacAMoleView.swift
+++ VisionTestApp/WhacAMoleView.swift
@@ -10,32 +10,44 @@ import RealityKit
 
 struct WhacAMoleView: View {
     @State private var spherePos: SIMD3<Float> = [0, 2, -2]
+    @State private var sphereSize: Float = 0.5
+    @State private var score: Int = 0
+    
     
     var body: some View {
-        RealityView{ content in
-            let sphere = ModelEntity(mesh: .generateSphere(radius: 0.5))
-            sphere.position = spherePos
-            sphere.generateCollisionShapes(recursive: true)
-            sphere.components.set(InputTargetComponent())
-            content.add(sphere)
+        VStack {
+            Text("Score: \(score)")
+                .font(.largeTitle)
+                .bold()
+                .padding()
             
-            
-        } update: { content in
-            if let sphere = content.entities.first {
+            RealityView { content in
+                let sphere = ModelEntity(mesh: .generateSphere(radius: sphereSize))
                 sphere.position = spherePos
+                sphere.generateCollisionShapes(recursive: true)
+                sphere.components.set(InputTargetComponent())
+                content.add(sphere)
+                
+                
+            } update: { content in
+                if let sphere = content.entities.first {
+                    sphere.position = spherePos
+                    sphere.transform.scale = SIMD3<Float>(repeating: sphereSize)
+                    
+                }
+                
             }
-            
+            .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
+                score += 1
+                sphereSize = Float.random(in: 0.2 ... 0.5)
+                spherePos = SIMD3<Float>(
+                    Float.random(in: -0.5 ... 0.5),
+                    Float.random(in: 0.5 ... 1.5),
+                    -2
+                )
+                
+            })
         }
-        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-            spherePos = SIMD3<Float>(
-                Float.random(in: -0.5 ... 0.5),
-                Float.random(in: 0.5 ... 1.5),
-                -2
-            )
-            
-        })
-        
-        
         
     }
 }
