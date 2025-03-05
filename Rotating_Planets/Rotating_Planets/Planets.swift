3b791f5794250d13fd72e9a38f3affd5a64956c4
Smrevilo <olivermstege@gmail.com>
2025-03-05 21:22:01 +0100

    Orbits done

--- Rotating_Planets/Rotating_Planets/Planets.swift
+++ Rotating_Planets/Rotating_Planets/Planets.swift
@@ -42,59 +42,32 @@ struct Planets: View {
         }
         .gesture(TapGesture().targetedToAnyEntity().onEnded { value in
             let tappedEntity = value.entity
-            
-            Task{
-                // 1. Calculate the transforms.
-                let initialTransform = tappedEntity.transform
+            if let planetSpin = tappedEntity.parent {
+                if let planetOrbit = planetSpin.parent {
+                    Task{
+                        let rotateFullSequence = Planets.createRotationAnimation(initialTransform: tappedEntity.transform, center: planetOrbit.transform.translation)
+                        
+                        let yAxis: SIMD3<Float> = [0, 1, 0]
 
-                // First target: 1/3 of a full rotation (120°) about the y-axis.
-                var transformTarget1 = initialTransform
-                let rotation120 = simd_quatf(angle: 2 * Float.pi / 3, axis: SIMD3<Float>(0,1,0))
-                transformTarget1.rotation = initialTransform.rotation * rotation120
+                        let orbit = OrbitAnimation(
+                            name: "orbit",
+                            duration: 50,
+                            axis: yAxis,
+                            startTransform: planetSpin.transform,
+                            spinClockwise: true,
+                            orientToPath: true,
+                            bindTarget: .transform,
+                            repeatMode: .repeat
+                        )
+                        
+                        if let orbitAnimationResource = try? AnimationResource.generate(with: orbit) {
+                            planetSpin.playAnimation(orbitAnimationResource, transitionDuration: 0.0, startsPaused: false)
+                        }
 
-                // Second target: 2/3 of a full rotation (another 120° from target1, total 240°)
-                var transformTarget2 = transformTarget1
-                transformTarget2.rotation = transformTarget1.rotation * rotation120
-                
-
-                // 2. Define the duration for each segment.
-                let duration: TimeInterval = 10
-
-                // 3. Create the animations.
-                let rotateSegment1 = FromToByAnimation(
-                    from: initialTransform,
-                    to: transformTarget1,
-                    duration: duration,
-                    timing: .linear,
-                    bindTarget: .transform
-                )
-
-                let rotateSegment2 = FromToByAnimation(
-                    from: transformTarget1,
-                    to: transformTarget2,
-                    duration: duration,
-                    timing: .linear,
-                    bindTarget: .transform,
-                    delay: duration  // start after the first animation finishes
-                )
-                
-                let rotateSegment3 = FromToByAnimation(
-                    from: transformTarget2,
-                    to: initialTransform,
-                    duration: duration,
-                    timing: .linear,
-                    bindTarget: .transform,
-                    delay: duration * 2  // start after the second animation finishes
-                )
-
-                // 4. Group them so they play in sequence and repeat.
-                let rotateFullSequence = AnimationGroup(
-                    group: [rotateSegment1, rotateSegment2, rotateSegment3],
-                    repeatMode: .repeat
-                )
-
-                if let rotationAnimation = try? AnimationResource.generate(with: rotateFullSequence) {
-                    tappedEntity.playAnimation(rotationAnimation, transitionDuration: 0.0, startsPaused: false)
+                        if let rotationAnimation = try? AnimationResource.generate(with: rotateFullSequence) {
+                            tappedEntity.playAnimation(rotationAnimation, transitionDuration: 0.0, startsPaused: false)
+                        }
+                    }
                 }
             }
             
@@ -102,6 +75,55 @@ struct Planets: View {
         .onAppear() {
             dismissWindow(id: "main")
         }
+    }
+    
+    static private func createRotationAnimation(initialTransform: Transform, center: SIMD3<Float>) -> AnimationGroup {
+        // First target: 1/3 of a full rotation (120°) about the y-axis.
+        var transformTarget1 = initialTransform
+        let rotation120 = simd_quatf(angle: 2 * Float.pi / 3, axis: SIMD3<Float>(0,1,0))
+        transformTarget1.rotation = initialTransform.rotation * rotation120
+
+        // Second target: 2/3 of a full rotation (another 120° from target1, total 240°)
+        var transformTarget2 = transformTarget1
+        transformTarget2.rotation = transformTarget1.rotation * rotation120
+        
+
+        // 2. Define the duration for each segment.
+        let duration: TimeInterval = 3
+
+        // 3. Create the animations.
+        let rotateSegment1 = FromToByAnimation(
+            from: initialTransform,
+            to: transformTarget1,
+            duration: duration,
+            timing: .linear,
+            bindTarget: .transform
+        )
+
+        let rotateSegment2 = FromToByAnimation(
+            from: transformTarget1,
+            to: transformTarget2,
+            duration: duration,
+            timing: .linear,
+            bindTarget: .transform,
+            delay: duration  // start after the first animation finishes
+        )
+        
+        let rotateSegment3 = FromToByAnimation(
+            from: transformTarget2,
+            to: initialTransform,
+            duration: duration,
+            timing: .linear,
+            bindTarget: .transform,
+            delay: duration * 2  // start after the second animation finishes
+        )
+
+        // 4. Group them so they play in sequence and repeat.
+        return AnimationGroup(
+            group: [rotateSegment1, rotateSegment2, rotateSegment3],
+            repeatMode: .repeat
+        )
+        
     }
 }
 
