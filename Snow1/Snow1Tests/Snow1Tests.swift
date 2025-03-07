9f261f09337d1798771341184a2b9c9146bc2430
Smrevilo <olivermstege@gmail.com>
2025-03-07 10:35:19 +0100

    collision shapes

---
+++ Snow1/Snow1Tests/Snow1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Snow1Tests.swift
+//  Snow1Tests
+//
+//  Created by m1 on 07/03/2025.
+//
+
+import Testing
+@testable import Snow1
+
+struct Snow1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
