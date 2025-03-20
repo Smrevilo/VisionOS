63d280050be5b997a29062e4644a7eda480e6e17
Smrevilo <olivermstege@gmail.com>
2025-03-20 12:28:41 +0100

    PortalBox

---
+++ PortalBox/PortalBox/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  PortalBox
+//
+//  Created by m1 on 20/03/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ PortalBox/PortalBox/PortalBoxApp.swift
@@ -0,0 +1,33 @@
+//
+//  PortalBoxApp.swift
+//  PortalBox
+//
+//  Created by m1 on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PortalBoxApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
