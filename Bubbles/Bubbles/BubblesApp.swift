5683a2eaa048859a48dac8777b75f27689d41c68
Smrevilo <olivermstege@gmail.com>
2025-03-22 15:11:23 +0100

    Shader Graph

---
+++ Bubbles/Bubbles/BubblesApp.swift
@@ -0,0 +1,33 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by m1 on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
